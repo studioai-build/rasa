f93487a1c5ead7d9c82d5fa8050a9ad1967e0e79
Mahmoud Khaled <107557835+mahmoudkhalled@users.noreply.github.com>
2024-01-07 16:44:40 +0200

    adding doc string

--- messages/messages.py
+++ messages/messages.py
@@ -2,6 +2,16 @@ from database.price_list import BENEFITS_PRICE
 
 
 def generate_message(member_id=None, benefits=None):
+    """
+    Generate a message based on the provided member ID and benefits.
+
+    Args:
+        member_id (str, optional): The member ID. Defaults to None.
+        benefits (str, optional): The benefits to be checked. Defaults to None.
+
+    Returns:
+        dict: A dictionary containing different types of messages based on the inputs.
+    """
     return {
         "prior_authorization" : f'Affirm Prior Authorization for member id: {member_id}',
         "prior_authorization_not_found" : "Please provide member id",
--- validation/validate_member_id.py
+++ validation/validate_member_id.py
@@ -3,13 +3,29 @@ from messages.messages import generate_message
 
 
 class MemberIDValidator:
+    """
+    A class to validate member IDs.
+
+    Methods:
+    - validate(member_id): Validates the provided member ID.
+    """
+
     def __init__(self):
         pass
 
     def validate(self, member_id):
+        """
+        Validates the provided member ID.
+
+        Parameters:
+        - member_id (str): The member ID to be validated.
+
+        Returns:
+        - str: The validation result message.
+        """
         if len(member_id) != 9:
-            return "Please provide valid member id of 9 digits"
+            return "Please provide a valid member ID of 9 digits"
         elif member_id in MEMBERS_LIST_ID:
-            return generate_message(member_id = member_id)["prior_authorization"]
+            return generate_message(member_id=member_id)["prior_authorization"]
         else:
-            return "Member id not found in database"
+            return "Member ID not found in the database"
